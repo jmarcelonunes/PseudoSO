0d0d841ac0a8afc443e00ed04be9af8255903791
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2019-11-10 17:47:47 -0300

    Memory structure comments

---
+++ memory.py
@@ -0,0 +1,7 @@
+'''
+Memory Module
+
+Contiguous blocks of memory. Each process have to check if there is memory avaiable before executing.
+64 blocks to real-time processes
+960 blocks to user processes
+'''
