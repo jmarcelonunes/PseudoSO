6614a5ec8b0196855f84748d4ee490eb02f85dae
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2019-11-10 18:33:37 -0300

    New memory class with inital funcitons for allocating memory

--- memory.py
+++ memory.py
@@ -5,3 +5,20 @@ Contiguous blocks of memory. Each process have to check if there is memory avaia
 64 blocks to real-time processes
 960 blocks to user processes
 '''
+
+class Memory:
+    def __init__(self):
+        self.initialMemory = 1024 #Check the memory word size?
+
+    def CheckAvailMemory(self, process):
+        
+        MemReq = process.numBlockMem #Size of the process
+
+        #Check process priority
+        if process.prioridade == '0': #Real-time process
+            InitialBlock = 0
+            FinalBlock = 63
+        else:
+            InitialBlock = 64
+            FinalBlock = 959
+
