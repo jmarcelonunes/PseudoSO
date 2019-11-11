5f8ca879f40b65b15877be18fc861226ce8a2ff5
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2019-11-11 00:20:28 -0300

    Checking memory function and allocate memory function

--- memory.py
+++ memory.py
@@ -8,7 +8,7 @@ Contiguous blocks of memory. Each process have to check if there is memory avaia
 
 class Memory:
     def __init__(self):
-        self.initialMemory = 1024 #Check the memory word size?
+        self.initialMemory = [None]*1024 #1024 blocks with null values
 
     def CheckAvailMemory(self, process):
         
@@ -18,14 +18,58 @@ class Memory:
         if process.prioridade == '0': #Real-time process
             InitialBlock = 0
             FinalBlock = 63
-        else:
+        else:                         #User process
             InitialBlock = 64
             FinalBlock = 959
-        
-        #To Do:
 
         #Check if the process is not too big to fit the blocks 
-
+        if MemReq > (FinalBlock - InitialBlock):
+            print("The process is too big to fit our memory")
+            return -1
 
         #Check if there is enough room for the process in the memory
+        y = 0
+        for x in range(InitialBlock, FinalBlock + 1):
+            
+            if self.initialMemory[x] == None: #Not occupied
+                
+                for y in range(x, FinalBlock + 1): 
+                    AvailSize = (y - x) 
+                    
+                    if y == FinalBlock: #If there is not enough space
+                        print("No sufficient memory")
+                        return -1
+
+                    if AvailSize == MemReq: #Check if the size is sufficient
+                        FinalBlockValue = y #Set the final allocation block
+                        break
+                    
+            if AvailSize == MemReq:
+                return FinalBlockValue
+            
+            if x == FinalBlock: #If there is none blank space
+                print("No sufficient memory")
+                return -1
+                        
+    def allocateMemory(self, process, FinalBlockValue):
+        InitialBlockValue = FinalBlockValue - process.numBlockMem
+        
+        for x in range(InitialBlockValue, FinalBlockValue):
+            self.initialMemory[x] = 1
+        
+        self.initialMemory[x + 1] = 1
+        
+        return 1
+
+    # TO DO:
+    #Clean memory
+    #Run this code and debug (Especially Check Avail Memory)
+    
+
+
+
+        
+                
+            
+        
 
