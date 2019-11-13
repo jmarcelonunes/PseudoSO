ea458818a0c364b30ecf772569a87f26d990e71f
nmschumacher <nicolasmschumacher@gmail.com>
2019-11-13 20:33:46 -0200

    parser update, process -> process_descr

--- parser.py
+++ parser.py
@@ -8,29 +8,28 @@
 		1.2) processos = read_processes('/home/aluno/SO/dados_processos.txt')
 """
 
-from processos import Process
-
 
 def read_processes(filename='processes.txt'):
-	"""Lê o txt informado e transforma em uma lista de processos.
+	"""Lê o txt informado e transforma em uma lista de descrição de processos.
 		Cada linha do arquivo deve seguir o padrão: int, int, int, int, int, int, int, int
 		sendo que, em ordem, cada valor int representa: <tempo de inicialização>, <priority>, 
 		<tempo de processador>, <blocos em memória>, <número-código da impressora requisitada>, 
 		<requisição do scanner>, <requisição do modem>, <número-código do disco>
 
 		Args:
-			filename ('string') Nome/caminho do arquivo texto para ser carregado
+			filename ('string') Nome/caminho do arquivo txt para ser carregado
 		Returns:
-			Process ('list')
+			processes_descr ('list') descrição dos processos em uma lista de dicionários
 	"""
-	processes = []
+	processes_descr = []
+	attrs = ['init_time', 'priority', 'total_exec_time', 'blocks', 'printer_cod', \
+				'req_scanner', 'req_modem', 'disk_cod']
 	with open(filename, 'r') as fp:
 		for i, line in enumerate(fp):
 			# remove \n, \r, \t e espaços que estejam no início ou no fim de cada linha
 			line = line.strip()
-			# d: dados de um processo. Nome suscinto de variável, mas prático para a operação seguinte.
-			d = line.split(',')
-			processes.append(Process(int(d[0]), int(d[1]), int(d[2]), \
-					int(d[3]), int(d[4]), int(d[5]), int(d[6]), int(d[7])) )
-	return processes
+			data = line.split(',')
+			process_info = {k: int(v) for k, v in zip(attrs, data)}
+			processes_descr.append(process_info)
+	return processes_descr
 
